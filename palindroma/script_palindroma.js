42f7050cfa15c72f8e0ff0ed2dd14ea0fe46534a
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-16 17:10:47 +0100

    add palindroma function

---
+++ palindroma/script_palindroma.js
@@ -0,0 +1,26 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+const wordInput = document.querySelector('input');
+
+const clickBtn = document.getElementById('btn_palindroma_1');
+const cancelBtn = document.getElementById('btn_palindroma_2');
+
+const ticket = document.getElementById('ticket');
+
+clickBtn.addEventListener('click', palindroma);
+function palindroma() {
+    let wordValue = wordInput.value;
+    let word = wordValue.split(" ").reverse().join(" ");
+    if (wordValue === word) {
+        ticket.innerHTML = `<h1>The word is a palindrome!</h1>`;
+        ticket.classList.remove ("d-none");
+}
+
+cancelBtn.addEventListener('click', palindroma);
+function palindroma() {
+    wordValue = '';
+    word = '';
+    ticket.classList.toggle ("d-none");
+}
