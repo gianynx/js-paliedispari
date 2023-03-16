aded5c657653a6a32145b519b7e5514545fc86f2
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-16 17:35:53 +0100

    modify script_palindroma.js

--- palindroma/script_palindroma.js
+++ palindroma/script_palindroma.js
@@ -12,12 +12,11 @@ const ticket = document.getElementById('ticket');
 clickBtn.addEventListener('click', palindroma);
 function palindroma() {
     let wordValue = wordInput.value.toLowerCase();
-    let word = wordValue.split(" ").reverse().join(" ");
+    let word = wordValue.split("").reverse().join("");
     if (wordValue === word) {
         ticket.innerHTML = `<h1>The word is a palindrome!</h1>`;
         ticket.classList.remove ("d-none");
     } else {
-        ticket.innerHTML = `<h1>The word isn't a palindrome!</h1>`;
         ticket.classList.remove("d-none");
     }
 }
